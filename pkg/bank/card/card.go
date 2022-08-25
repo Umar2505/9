36d710887750563c41cc7fe9130d763d30cea944
Umar <rustamovumar0@gmail.com>
2022-08-25 13:23:57 +0500

    types and package card created

---
+++ pkg/bank/card/card.go
@@ -0,0 +1,11 @@
+package bank
+
+import (
+	"bank/pkg/bank/types"
+)
+
+func PaymentSources(cards []types.Card) []types.PaymentSource {
+	result:= make([]types.PaymentSource,3)
+	return result
+}
+   
